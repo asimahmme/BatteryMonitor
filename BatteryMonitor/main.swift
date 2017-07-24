2e57904a91c7b5cc887205d07e0f29b1043911fc
Asim Ahmed <asimahmed@asims-mbp>
2017-07-24 12:51:36 -0700

    Initial Commit

---
+++ BatteryMonitor/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BatteryMonitor
+//
+//  Created by Asim Ahmed on 7/24/17.
+//  Copyright © 2017 Asim Ahmed. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
